8a49dd2c4d37f44a33706cdd1ba5ce02d7ddd881
Christopher Kelling <cj.kelling88@gmail.com>
2020-07-13 18:28:00 -0600

    Add attribute validation and relationship tests to Offer model.

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,4 +1,5 @@
 class Offer < ActiveRecord::Base
+  validates_presence_of :name, :description, :terms, :image_url, :expiration
   has_many :retailer_offers
   has_many :retailers, through: :retailer_offers
 end
