c67fb5cb74ee86d101544c80b46c9aa74cd70a08
Christopher Kelling <cj.kelling88@gmail.com>
2020-07-17 09:51:13 -0600

    Made Header a component for Index. Made logo an asset versus calling out

--- vue_files/src/components/Index.vue
+++ vue_files/src/components/Index.vue
@@ -1,14 +1,7 @@
 <template>
   <div id="app">
 
-    <header>
-      <img class='logo-image' :src='logo'>
-      <ul v-if="errors">
-        <li v-for="error of errors">
-          {{error.message}}
-        </li>
-      </ul>
-    </header>
+    <Header/>
 
     <main>
       <div class="gallery" v-if="gallery">
@@ -51,6 +44,7 @@
 </template>
 
 <script>
+  import Header from './Header.vue';
   import axios from 'axios';
 
   export default{
@@ -61,7 +55,6 @@
         gallery: true,
         offerId: null,
         search: '',
-        logo: 'https://home.ibotta.com/wp-content/uploads/2019/06/IbottaLogo_Primary_Pink.png',
         endpoint: 'http://localhost:3000/api/v1/offers'
       }
     },
@@ -97,89 +90,89 @@
         var day = data.slice(8, 10);
         return month + '/' + day + '/' + year
       }
+    },
+    components: {
+      Header
     }
   }
 </script>
 
 <style scoped>
-.logo-image {
-  width: 15%;
-}
-.gallery {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-evenly;
-  margin: 3rem;
-}
+  .gallery {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+    margin: 3rem;
+  }
 
-.offer-card {
-  display: inline-block;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  position: relative;
-  background-color: white;
-  width: 15rem;
-  height: 18rem;
-  padding: 0.8rem;
-  margin: 0.5rem;
-  border: 1px solid rgba(0, 0, 0, 0.1);
-  border-radius: 0.5rem;
-  box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2);
-  transition: 0.2s;
-  cursor: pointer;
-}
+  .offer-card {
+    display: inline-block;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    position: relative;
+    background-color: white;
+    width: 15rem;
+    height: 18rem;
+    padding: 0.8rem;
+    margin: 0.5rem;
+    border: 1px solid rgba(0, 0, 0, 0.1);
+    border-radius: 0.5rem;
+    box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2);
+    transition: 0.2s;
+    cursor: pointer;
+  }
 
-.image-container {
-  width: 13rem;
-  height: 10rem;
-}
+  .image-container {
+    width: 13rem;
+    height: 10rem;
+  }
 
-.description-container {
-  width: 13rem;
-  height: 8rem;
-}
+  .description-container {
+    width: 13rem;
+    height: 8rem;
+  }
 
-.offer-card:hover {
-  transform: scale(1.01);
-  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.19);
-}
+  .offer-card:hover {
+    transform: scale(1.01);
+    box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.19);
+  }
 
-.image {
-  width: 100%;
-  height: 100%;
-  object-fit: contain;
-}
+  .image {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
 
-.description {
-  object-fit: contain;
-}
+  .description {
+    object-fit: contain;
+  }
 
-.offer-card-detailed-container{
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
+  .offer-card-detailed-container{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 
-.offer-card-detailed {
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  position: relative;
-  background-color: white;
-  width: 75vw;
-  height: 75vh;
-  padding: 0.8rem;
-  margin: 0.5rem;
-  border: 1px solid rgba(0, 0, 0, 0.2);
-  border-radius: 0.5rem;
-  box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.2), 0 0 8px 0 rgba(0, 0, 0, 0.19);
-}
+  .offer-card-detailed {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    position: relative;
+    background-color: white;
+    width: 75vw;
+    height: 75vh;
+    padding: 0.8rem;
+    margin: 0.5rem;
+    border: 1px solid rgba(0, 0, 0, 0.2);
+    border-radius: 0.5rem;
+    box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.2), 0 0 8px 0 rgba(0, 0, 0, 0.19);
+  }
 
-.image-container-detailed {
-  width: 15rem;
-  height: 15rem;
-}
+  .image-container-detailed {
+    width: 15rem;
+    height: 15rem;
+  }
 </style>
